c665e972d6fed4058dbe93edf7922d3211df486c
chep <chepby@gmail.com>
2018-01-16 19:48:30 +0300

    Add sout in main

--- New/NewClass.java
+++ New/NewClass.java
@@ -1,6 +1,6 @@
 public class NewClass {
     public static void main(String[] args) {
-        System.out.println("helloWorld");
+        System.out.println("new day");
         System.out.println("try to marge");
     }
 }
