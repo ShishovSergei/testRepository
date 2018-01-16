c1d0e8171df643fc90b67eda821a719120aa68aa
chep <chepby@gmail.com>
2018-01-16 20:55:57 +0300

    add sout

--- New/NewClass.java
+++ New/NewClass.java
@@ -1,7 +1,8 @@
 public class NewClass {
     public static void main(String[] args) {
         System.out.println("new day hello");
-        System.out.println("try to marge");
-        System.out.println("ggfgds");
+        System.out.println("try to marge ");
+        System.out.println("ggfgds ");
+        System.out.println("sdfdsfsdf");
     }
 }
