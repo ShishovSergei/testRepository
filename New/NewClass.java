d3a4cc30f39198f8a673a96627ce80d2cd10e29a
chep <chepby@gmail.com>
2018-01-16 20:25:19 +0300

    new branch

--- New/NewClass.java
+++ New/NewClass.java
@@ -1,6 +1,6 @@
 public class NewClass {
     public static void main(String[] args) {
-        System.out.println("new day");
+        System.out.println("new day hello");
         System.out.println("try to marge");
         System.out.println("ggfgds");
     }
