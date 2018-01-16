f727cfb9c3930317fd71f0a1355c034edcac6243
chep <chepby@gmail.com>
2018-01-16 20:37:56 +0300

    dgsdf

--- New/NewClass.java
+++ New/NewClass.java
@@ -1,7 +1,8 @@
 public class NewClass {
     public static void main(String[] args) {
         System.out.println("last day hello");
-        System.out.println("try to merge");
+        System.out.println("try to merge ");
         System.out.println("ggfgds");
+        System.out.println("fdkjfdskjhsfdf");
     }
 }
