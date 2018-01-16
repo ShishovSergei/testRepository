e0e638d82123c869a1f00aa311d2a732ea9a1125
chep <chepby@gmail.com>
2018-01-16 20:26:12 +0300

    new branch

--- New/NewClass.java
+++ New/NewClass.java
@@ -1,6 +1,6 @@
 public class NewClass {
     public static void main(String[] args) {
-        System.out.println("last day");
+        System.out.println("last day hello");
         System.out.println("try to merge");
         System.out.println("ggfgds");
     }
