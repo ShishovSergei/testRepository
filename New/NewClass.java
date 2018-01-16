94d7645c669e694a6a85ce04a5e0766a257180b6
ShishovSergei <shishovsergeiwork@gmail.com>
2018-01-16 19:53:41 +0300

    message

--- New/NewClass.java
+++ New/NewClass.java
@@ -2,5 +2,6 @@ public class NewClass {
     public static void main(String[] args) {
         System.out.println("new day");
         System.out.println("try to marge");
+        System.out.println("ggfgds");
     }
 }
